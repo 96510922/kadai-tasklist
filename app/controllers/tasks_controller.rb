2351b7001f3b94f97c807abaaa5c380208d743d9
naoya.takeda <tn.soccerball.99@gmail.com>
2020-12-21 12:30:20 +0000

    kadai-15

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,17 @@
 class TasksController < ApplicationController
+    
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
+
+    
     def index
-        @tasks = Task.all
+       @tasks = Task.all#.page(params[:page]).per(3)
+        
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
+        
     end
     
     def show
@@ -12,6 +23,7 @@ class TasksController < ApplicationController
     end
     
     def create 
+=begin    
         @task = Task.new(task_params)
         
         if @task.save
@@ -22,7 +34,17 @@ class TasksController < ApplicationController
             flash.now[:danger] = "タスクが投稿されませんでした"
             render :new
         end
-            
+=end
+        
+         @task = current_user.tasks.build(task_params)
+        if @task.save
+          flash[:success] = 'メッセージを投稿しました。'
+          redirect_to root_url and return
+        else
+          @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+          flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+          render 'tasks/index'
+        end    
     end
     
     def edit
@@ -42,11 +64,16 @@ class TasksController < ApplicationController
     end
     
     def destroy
+=begin
         @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'タスク は正常に削除されました'
         redirect_to tasks_url
+=end       
+         @task.destroy
+        flash[:success] = 'メッセージを削除しました。'
+        redirect_back(fallback_location: root_path)
     end
     
       private
@@ -56,5 +83,11 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
-   
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end 
 end
